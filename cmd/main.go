b6d065c7711ef189469c80aebf2bfcdaf6d5385a
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-07-05 13:44:27 -0400

    create backend branch

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
-
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Hello, World!")
 }
